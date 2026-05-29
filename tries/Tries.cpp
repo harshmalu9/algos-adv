38a137b93b0340c14ad83578596daeca1e89f858
harshmalu9 <hmalu975@gmail.com>
2026-05-29 14:23:21 +0530

    started with tries

---
+++ tries/Tries.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+const int MOD = 1e9 + 7;
+
+void solve() {
+    
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); 
+    cout.tie(NULL);
+
+    int _t = 1;
+    // cin >> _t;
+    while(_t--) {
+        solve();
+    }
+
+    return 0;
+}

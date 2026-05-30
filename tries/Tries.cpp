328394321d33d92d5b89df10bad1ae97d524867c
harshmalu9 <hmalu975@gmail.com>
2026-05-30 14:41:29 +0530

    tries code

--- tries/Tries.cpp
+++ tries/Tries.cpp
@@ -4,8 +4,101 @@ using namespace std;
 
 const int MOD = 1e9 + 7;
 
+struct Node{
+    Node* mark[26];
+    bool flag;
+
+    Node(){
+        for(int i=0; i<26; i++) mark[i] = NULL;
+        flag = false;
+    }
+
+    bool containsKey(char ch){
+        return (mark[ch - 'a'] != NULL);
+    }
+
+    void put(char ch, Node* node){
+        mark[ch-'a'] = node;
+    }
+
+    Node* getNext(char ch){
+        return mark[ch - 'a'];
+    }
+
+    void setEnd(){
+        flag = true;   
+    }
+};
+
+class Trie{
+private:
+    Node* root;
+
+public: 
+    Trie(){
+        root = new Node;
+    }
+
+    void insert(string word) {
+        Node* node = root;
+
+        for(int i=0; i<word.size(); i++){
+
+            if(!node->containsKey(word[i])){
+                node->put(word[i], new Node());
+            }
+
+            node = node->getNext(word[i]);  
+        }
+
+        node->setEnd();  
+    }
+
+    bool search(string word){
+        Node* node = root;
+
+        for(int i=0; i<word.size(); i++){
+
+            if(!node->containsKey(word[i])) return false;
+            
+            node = node->getNext(word[i]);
+        }
+
+        return node->flag;
+    } 
+
+    bool startsWith(string prefix){
+        Node* node = root;
+
+        for(int i=0; i<prefix.size(); i++){
+            if(!node->containsKey(prefix[i])) return false;
+            
+            node = node->getNext(prefix[i]);
+        }
+
+        return true;
+    }
+
+};
+
 void solve() {
-    
+    Trie t = Trie();
+
+    t.insert("harsh");
+    t.insert("harmonica");
+    t.insert("hxxx");
+
+    cout << t.search("xyz") << '\n';
+    cout << t.search("harsh") << '\n';
+    cout << t.search("har") << '\n';
+
+    cout << "\n\n" ;
+
+    cout << t.startsWith("har") << '\n';
+    cout << t.startsWith("hxx") << '\n';
+    cout << t.startsWith("h") << '\n';
+    cout << t.startsWith("s") << '\n';
+
 }
 
 signed main(){
